fc59039144cba6fca8f0226d1691cd085c520717
Kamila Quintana <kami.quintana@vintencloud.com>
2021-11-04 20:10:14 -0300

    naxa

--- clase02/app/app.js
+++ clase02/app/app.js
@@ -2,11 +2,22 @@ const app = new Vue({
     el: "#app",
     data: {
         titulo: "App en vue",
-        cedula: ''
+        cedula: '',
+        resultado: ''
     },
     methods: {
         buscarPersona: function (){
             console.log("cedula", this.cedula)
+            for (let i = 0; i < personas.length; i++) {
+                if (this.cedula === personas[i].cedulaPer) {
+                    this.resultado = this.cedula
+                }
+            }
         }
     }
 })
+
+const personas = [
+    {nombre: 'Juan',
+    cedulaPer: '123'}
+]
