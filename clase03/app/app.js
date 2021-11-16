20d0b3b8a3f4bdceee906817d0017f26561485b2
Alex Casadevall <alex@lexartlabs.com>
2021-11-15 21:18:30 -0300

    add contador completados

--- clase03/app/app.js
+++ clase03/app/app.js
@@ -3,19 +3,37 @@ const app = new Vue({
     data: {
         item: '',
         lista: [],
-        flag: true
+        flag: true,
+        total: 0,
+        completados: 0
     },
     methods: {
         agregar: function (){
             this.lista.push({nombre: this.item, completado: false})
             this.item = ''
+
+            this.totalizar()
         },
         eliminar: function (indice){
             this.lista.splice(indice, 1)
+
+            this.totalizar()
         }, 
         completar: function (indice){
             this.lista[indice].completado = true
             console.log("this.lista: ", this.lista)
+
+            this.totalizar()
+        },
+        totalizar: function (){
+            this.total = this.lista.length
+            let count = 0
+            this.lista.map( item => {
+                if(item.completado === true){
+                    count++
+                }
+            })
+            this.completados = count
         }
     }
 })
